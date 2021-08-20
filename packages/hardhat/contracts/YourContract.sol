18025487c40758d02e880036f0149601645eec1f
ShuGe-MIT <71540484+ShuGe-MIT@users.noreply.github.com>
2021-08-20 00:01:33 -0400

    Add files via upload

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,22 +1,28 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
+// import "./hashVerifier.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
+
+// `commitToNumberAndThreshold(uint256 commitment)`
+//  - check that `playerCommit == 0`
+//  - updates `playerCommit` to `commitment`. Sets `threshold = blockhash(block.number - 1)`
+
+
 contract YourContract {
-
-  //event SetPurpose(address sender, string purpose);
-
-  string public purpose = "Building Unstoppable Apps";
-
-  constructor() {
-    // what should we do on deploy?
+  uint256 playerCommit = 0;
+  uint256 threshold;
+  constructor() public {
   }
-
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      //emit SetPurpose(msg.sender, purpose);
+  function setThreshold(uint commitment) public {
+        require(playerCommit == 0, "You have already played.");
+        require(1<=commitment && commitment<=13, "Invalid card.");
+        uint block_hash = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+        playerCommit = commitment;
+        threshold = block_hash % 13 + 1;
   }
 }
